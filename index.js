27ce1ba86918877d43fcc1510b6c0e8cb253ec6d
rik <ingeniero_rik@hotmail.com>
2024-04-28 00:36:16 -0300

    se modifica index.ejs

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require ("express");
 const app = express();
 const path = require('path');
 const fs = require("fs");
-const cors = require ('cors') // <--------- para que puedan ver los datos de nuestra api  hacer  --> npm i cors
+const cors = require ('cors'); // <--------- para que puedan ver los datos de nuestra api  hacer  --> npm i cors
+//const fotosController = require("./controllers/fotosController");
 
 app.use(express.json());
 
-
-
 app.use(cors());
-
 
 // indicamos a express usar la plantilla EJS que esta en carpeta views.
 app.set('view engine', 'ejs');
@@ -23,10 +21,12 @@ app.use(express.static('public'));
 
 const fotosFilePath = path.join(__dirname, "data/db_fotos.json");
 const db_fotos = JSON.parse(fs.readFileSync(fotosFilePath, "utf-8"));
+/* En la constante "db_fotos" ya tenemos los fotos que están 
+guardados en la carpeta data como Json (un array de objetos literales) */
 
-app.get("/", (req,res) => {
-    
-    res.render("index.ejs");
+app.get("/", (req,res) =>{
+
+    res.render("index.ejs", { fotos : db_fotos }); 
 });
 
 app.get("/api/fotos", (req,res) => {
@@ -41,12 +41,12 @@ app.get("/api/fotos/:id", (req,res) => {
     else res.send(foto);
 });
 
+
 app.get("/api/fotos/:id/img", (req, res) => {
     const foto = db_fotos.find(c => c.id === parseInt(req.params.id));
     if (!foto) return res.status(404).send("Foto no encontrada");
     else res.sendFile(path.join(__dirname, 'public', 'images', foto.img));
 });
-
 
 // ponemos a escuchar el servidor
 app.listen(process.env.PORT || 3042, () =>  // si subimos a un hosting este nos dará el puerto, sinó sera 3041
